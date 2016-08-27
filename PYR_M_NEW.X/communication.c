6bac53d54f819c49a631f10169d9651e192b8b52
LucasUllrich <lucas.ullrich@aon.at>
2016-08-27 09:35:16 +0200

    Add comments

--- PYR_M_NEW.X/communication.c
+++ PYR_M_NEW.X/communication.c
@@ -2,13 +2,18 @@
 
 void Transmit(unsigned char slave, unsigned char trans_data) {
     Transmit_En = 1;
-    RCSTA1bits.CREN = 0;
+    RCSTA1bits.CREN = 0;            // Disabled reception to counteract
+                                    // interferences by a simultanious
+                                    // reception while sending
     while(PIR1bits.TXIF == 0);
     TXSTA1bits.TX9D = 1;
     TXREG1 = slave;
     while(PIR1bits.TXIF == 0);
     TXREG1 = trans_data;
-    while(TXSTA1bits.TRMT == 0);
+    while(TXSTA1bits.TRMT == 0);    // Verry important to be TRMT
+                                    // This bit indicates an empty
+                                    // Transmit register, not an
+                                    // empty buffer
     Transmit_En = 0;
 #ifdef COM_TEST
     RCSTA1bits.CREN = 0;
---
+++ Readme.md
@@ -0,0 +1,5 @@
+Master Branch:
+
+Communication Branch:
+File-path to test the communication of the different devices.
+Contains code which only enables the com and send dummy data which toggles a LED of the recipient.
